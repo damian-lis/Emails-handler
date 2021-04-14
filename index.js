7d7c05429446d7478d7f9f390553c49fc9303f1a
Damian Lis <foxprogramming93@gmail.com>
2021-04-14 20:13:12 +0200

    Created new routes paths in main index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const sendMail = require('./config/sendGrid/index.js')
+const sendGisapiaMail = require('./config/sendGrid/index.js')
+const sendPortfolioMail = require('./config/nodemailer/index.js')
 const cors = require('cors')
 
 const app = express()
@@ -8,7 +9,8 @@ const port = process.env.PORT || '5000'
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/mail', sendMail)
-app.get('/', (req, res) => res.send('Email handler app'))
+app.use('/api/mail/gisapia', sendGisapiaMail)
+app.use('/api/mail/portfolio', sendPortfolioMail)
+app.get('/', (req, res) => res.send('Emails handler app'))
 
 app.listen(port, () => console.log(`Server running on ${port}`))
