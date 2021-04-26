a9349712d5f8f84c565e5762166d2c4d978a85cd
Damian Lis <foxprogramming93@gmail.com>
2021-04-26 13:47:26 +0200

    Moved create email message to frontend

--- config/sendGrid/index.js
+++ config/sendGrid/index.js
@@ -1,27 +1,24 @@
 const sgMail = require('@sendgrid/mail')
-const createGisapiaMessages = require('./createGisapiaMessages.js')
 const keys = require('../../env.js')
 
 sgMail.setApiKey(keys.SENDGRID_API_KEY || process.env.SENDGRID_API_KEY)
 
 const sendGisapiaMail = (req, res) => {
-  const mailMessage = createGisapiaMessages({ type: 'mail', data: req.body })
+  const charEmail = req.body
 
   sgMail
-    .send(mailMessage)
+    .send(charEmail)
     .then((response) => {
       console.log(response[0].statusCode)
       console.log(response[0].headers)
       res.json({
         success: true,
-        message: createGisapiaMessages({ type: 'success' }),
       })
     })
     .catch((error) => {
       console.error(error)
       res.json({
         success: false,
-        message: createGisapiaMessages({ type: 'error' }),
       })
     })
 }
